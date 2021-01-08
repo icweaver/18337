adbcfedb0cef8e9e37319bfd9760b9e04f535738
icweaver <iweaver@cfa.harvard.edu>
2021-01-08 09:36:28 -0500

    L2 notes

---
+++ notebooks/scratch.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.12.18
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 47d7e091-f3f1-482f-9597-9bfd34ec2233
+md"""
+There is a disconnect between the theoretical CS world and the real world. For example:
+
+	Linked lists are actually not the bee's knees
+
+Why? Because hardware matters. Here is a mental model of the memory layout for a modern, multi-core computer:
+![](https://hackernoon.com/hn-images/1*nT3RAGnOAWmKmvOBnizNtw.png)
+
+"""
+
+# ╔═╡ Cell order:
+# ╠═47d7e091-f3f1-482f-9597-9bfd34ec2233
