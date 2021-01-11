4ff8b6eca15df430da6f796a6936c14bf509add9
icweaver <iweaver@cfa.harvard.edu>
2021-01-10 22:07:38 -0500

    updates

--- notebooks/scratch.jl
+++ notebooks/scratch.jl
@@ -10,6 +10,9 @@ using StaticArrays
 # ╔═╡ f8357564-8cf5-4329-86da-ea182471e0c9
 using Base.Threads
 
+# ╔═╡ 0b809325-6ac4-4326-8e07-0b0324fd3a6d
+using Statistics
+
 # ╔═╡ 8b6135cb-32a9-4849-8fde-e6d8b49a4fc9
 using PlutoUI, BenchmarkTools
 
@@ -226,7 +229,93 @@ end
 # ╔═╡ 757aa797-177e-4e45-8a27-2e857ba17e38
 md"""
 Oof, that's bad. By like three orders of magnitude. A major reason why goes back to the [famous chart](http://ithare.com/infographics-operation-costs-in-cpu-clock-cycles/). Creating threads is a relatively heavy CPU operation (Allocation+deallocation pair) compared to the handful of floating point operations ("Simple register-register op") we are doing per thread. In other words, mult-threading is completely overkill for this specific problem and is not worth the additional overhead incurred by just setting up the threads in the first place.
+
+The moral of the story is that multi-threading is not a silver bullet. You need to know the nature of the problem to know which tools will work best to solve it. As a rule of thumb, if an op takes a couple hundred nanosecond or less, it's probably overkill to try and multithread it.
+
+With that in mind, let's turn to a similar problem that is much more amenable to parallelization: Parameter Searches.
 """
+
+# ╔═╡ 51e88fd0-e199-40fa-add1-a982db7bd27c
+md"""
+# Multithreaded Parameter Searches
+"""
+
+# ╔═╡ cdb8aac6-232d-470c-98aa-839d93fc20fc
+const _u_cache = Vector{typeof(@SVector([1.0, 0.0, 0.0]))}(undef, 1_000)
+
+# ╔═╡ 8596f5af-3696-4d22-a847-49b76ca6a373
+function _compute_trajectory_mean(u, u0, p)
+	solve_system_save!(u, lorenz, u0, p, 1_000)
+	return mean(u)
+end
+
+# ╔═╡ 82543bf0-1b36-4511-8651-87d4492169cc
+# Using a closure to keep the API uniform. Also avoids passing in a global const
+compute_trajectory_mean(u0, p) = _compute_trajectory_mean(_u_cache, u0, p)
+
+# ╔═╡ b2f35677-cf25-42e3-8fa6-124d0186d0a7
+with_terminal() do
+	@btime compute_trajectory_mean(@SVector([1.0, 0.0, 0.0]), p)
+end
+
+# ╔═╡ b77c5a71-e2d8-4a2d-bcd0-8c9000e21033
+md"""
+Looks good, now let's search different inputs for `p`:
+"""
+
+# ╔═╡ 7a3385b5-eb08-4625-8268-348a002132b7
+ps = [(0.02, 10.0, 28.0, 8/3) .* (1.0, rand(3)...) for i in 1:1_000]
+
+# ╔═╡ c948a3b4-0af1-44cb-8300-331baf343581
+serial_out = map(
+	p -> compute_trajectory_mean(@SVector([1.0, 0.0, 0.0]), p),
+	ps
+)
+
+# ╔═╡ daa14169-be2b-4802-9e95-7b309935f4e5
+md"""
+Now multithreading:
+"""
+
+# ╔═╡ 1425adbd-6aab-4a41-85c3-95c85acf1bd0
+function tmap(f, ps)
+	out = Vector{typeof(@SVector([1.0, 0.0, 0.0]))}(undef, 1_000)
+	Threads.@threads for i in 1:1_000
+		out[i] = f(ps[i])
+	end
+	return out
+end
+
+# ╔═╡ 50a5f536-eeee-4053-8405-603c739bbf35
+threaded_out = tmap(
+	p -> compute_trajectory_mean(@SVector([1.0, 0.0, 0.0]), p),
+	ps
+)
+
+# ╔═╡ c88c7ea8-b6c7-4a6c-af16-8d01ab8ebcf6
+md"""
+Let's compare to the serial output:
+"""
+
+# ╔═╡ 03db62b2-ff33-4e3c-9c6c-0943fbd9cc19
+serial_out - threaded_out
+
+# ╔═╡ e81bdefd-77e3-4b23-9ade-c9f8ee25535c
+md"""
+Oh no, they're not the same! The problem is our cache vector, `_u_cache`. In serial, this is fine, but in parallel the threads just keep overwritting each other because they are all sharing that single cache. The process is not **thread safe**. We can fix this by giving each thread its own cache:
+"""
+
+# ╔═╡ 746db25d-f176-42d8-bec7-b9c9e159d5c6
+const _u_cache_threads = [
+	Vector{typeof(@SVector([1.0, 0.0, 0.0]))}(undef, 1_000)
+	for i in 1:Threads.nthreads()
+]
+
+# ╔═╡ 1c08555c-ba4d-405e-a7de-4f53656556e9
+
+
+# ╔═╡ ccf164c4-c190-4f07-94e9-c48dd2a9ea1e
+
 
 # ╔═╡ Cell order:
 # ╟─b51b7e9b-df76-4e57-83e6-03f78e0b1482
@@ -265,5 +354,23 @@ Oof, that's bad. By like three orders of magnitude. A major reason why goes back
 # ╠═f8357564-8cf5-4329-86da-ea182471e0c9
 # ╠═c2326f0b-356f-4353-b1e4-98960b4f4903
 # ╠═d386b7f7-a4e8-41b6-a8be-b26cc657db85
-# ╠═757aa797-177e-4e45-8a27-2e857ba17e38
+# ╟─757aa797-177e-4e45-8a27-2e857ba17e38
+# ╟─51e88fd0-e199-40fa-add1-a982db7bd27c
+# ╠═0b809325-6ac4-4326-8e07-0b0324fd3a6d
+# ╠═cdb8aac6-232d-470c-98aa-839d93fc20fc
+# ╠═8596f5af-3696-4d22-a847-49b76ca6a373
+# ╠═82543bf0-1b36-4511-8651-87d4492169cc
+# ╠═b2f35677-cf25-42e3-8fa6-124d0186d0a7
+# ╟─b77c5a71-e2d8-4a2d-bcd0-8c9000e21033
+# ╠═7a3385b5-eb08-4625-8268-348a002132b7
+# ╠═c948a3b4-0af1-44cb-8300-331baf343581
+# ╟─daa14169-be2b-4802-9e95-7b309935f4e5
+# ╠═1425adbd-6aab-4a41-85c3-95c85acf1bd0
+# ╠═50a5f536-eeee-4053-8405-603c739bbf35
+# ╟─c88c7ea8-b6c7-4a6c-af16-8d01ab8ebcf6
+# ╠═03db62b2-ff33-4e3c-9c6c-0943fbd9cc19
+# ╟─e81bdefd-77e3-4b23-9ade-c9f8ee25535c
+# ╠═746db25d-f176-42d8-bec7-b9c9e159d5c6
+# ╠═1c08555c-ba4d-405e-a7de-4f53656556e9
+# ╠═ccf164c4-c190-4f07-94e9-c48dd2a9ea1e
 # ╟─8b6135cb-32a9-4849-8fde-e6d8b49a4fc9
